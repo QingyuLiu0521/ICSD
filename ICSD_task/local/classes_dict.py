034779d966dd0c49f27456b6dafa2305cb638202
QingyuLiu0521 <121142410+QingyuLiu0521@users.noreply.github.com>
2024-07-25 21:24:29 +0800

    Add files via upload

---
+++ ICSD_task/local/classes_dict.py
@@ -0,0 +1,12 @@
+"""
+we store here a dict where we define the encodings for all classes in DESED task.
+"""
+
+from collections import OrderedDict
+
+classes_labels = OrderedDict(
+    {
+        "Infantcry": 0,
+        "Snoring": 1,
+    }
+)
